27a349f97508d34884670d3238ddc00290669970
Shreyas Mahajan <35132811+Shreyas9991@users.noreply.github.com>
2018-10-26 21:37:03 +0530

    Add files via upload

---
+++ rescue-bot/E-quake-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=e-quake-Web-Deploy.pubxml -p:Password=Xd85h0lL9ix7ZFF09Alf8xvYrsqe70ZSKxQzxfyr1Jr6tfwZXfPAeKYgxq1x
+
